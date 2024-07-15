297f4b36ecfaf98d7b52f95a682a562c7b0d969d
senthamizh-cse <senthamizh676@gmail.com>
2024-07-16 00:20:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'; 
+import TodoList from './TodoList';
+ 
+
+const App = () => {
+  return (
+    <div className="App">
+     <TodoList></TodoList>
+    </div>
+  );
+};
+
+export default App;
